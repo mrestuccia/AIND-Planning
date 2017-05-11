9145168323177ff559607955c1ab7e67650e3483
Mauro Restuccia <mrestuccia@mac.com>
2017-05-11 17:14:53 -0400

    raw results and script to compare them

---
+++ runner.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+number=1
+while [ $number -le 3 ]; do
+    number2=1
+    while [ $number2 -le 10 ]; do
+        echo "problem: $number - search: $number2"
+        python run_search.py -p $number -s $number2 >> result.txt
+        number2=$((number2 + 1))
+    done
+    number=$((number + 1))
+done
