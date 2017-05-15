27d0879c606e2dcfa480c7db17e7903d10217352
EC2 Default User <ec2-user@ip-172-31-50-6.ec2.internal>
2017-05-15 17:59:39 +0000

    final results

--- runner.sh
+++ runner.sh
@@ -5,7 +5,7 @@ while [ $number -le 3 ]; do
     number2=1
     while [ $number2 -le 10 ]; do
         echo "problem: $number - search: $number2"
-        python run_search.py -p $number -s $number2 >> result.txt
+        python3 run_search.py -p $number -s $number2 >> result.txt
         number2=$((number2 + 1))
     done
     number=$((number + 1))
